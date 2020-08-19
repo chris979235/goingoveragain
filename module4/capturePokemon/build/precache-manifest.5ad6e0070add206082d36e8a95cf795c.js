6c3e8036461b0a6d277fc8ec54daea9e2bfd936a
christopher rees <crees979@gmail.com>
2020-08-19 09:31:00 -0600

    updated

---
+++ module4/capturePokemon/build/precache-manifest.5ad6e0070add206082d36e8a95cf795c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d35ba4c3d56622cf5739f2722702d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fede0d1501b7b63def",
+    "url": "/static/css/main.2ccaefa6.chunk.css"
+  },
+  {
+    "revision": "5b7741cd44dfab815a15",
+    "url": "/static/js/2.d0b7233e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0b7233e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fede0d1501b7b63def",
+    "url": "/static/js/main.42277b8a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "7e389b68657a00f7b8d5a98c2c75558d",
+    "url": "/static/media/fight.7e389b68.jpg"
+  },
+  {
+    "revision": "d54a4378655d14d9dc4284f4f9c8c163",
+    "url": "/static/media/forest.d54a4378.jpg"
+  }
+]);
